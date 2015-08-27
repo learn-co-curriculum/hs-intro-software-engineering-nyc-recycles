b41c28f0e46c29c181cef31d8226ea58da4fc0e3
vicfriedman <victoria.m.friedman@gmail.com>
2015-08-27 10:32:21 -0400

    done

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,39 +1,34 @@
-# require 'open-uri'
-# require 'json'
-require 'pry'
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE  
-
-  
-#This method gets all the data from the NYC public recycling bin list and displays it as one huge string and its really messy and gross 
-# def raw_data
-#   open("https://data.cityofnewyork.us/resource/sxx4-xhzg.json").read
-# end
-
-#this method takes that big messy string 
-#and turns it into an array of hashes
-# def parsed_data
-#   JSON.parse(raw_data)
-# end
-
-# first_recycling_bin (and all the other recycling_bin hashes) is a variable storing a hash that looks like this:
-
-# {"address"=>"E 227 St/Bronx River Pkway",
-#  "site_type"=>"Subproperty",
-#  "longitude"=>"-73.864223918",
-#  "borough"=>"Bronx",
-#  "latitude"=>"40.890848989",
-#  "park_site_name"=>"227th St. Plgd"}
-
-
-# first_recycling_bin = parsed_data[0]
-# second_recycling_bin = parsed_data[1]
-# third_recycling_bin = parsed_data[2]
-# fourth_recycling_bin = parsed_data[3]
-
-
 ###### Code Your Solutions Here!!!
 
+#Your code should work with a hash that looks like the one below (in comments).
+#The specs (which call your tests) are using these exact hashes 
+# We haven't hard coded the hashes in the tests, 
+#they're dynamically brought in from the NYC Open Data API
+
+# first_recycle_bin =  {
+#   "address"=>"E 227 St/Bronx River Pkway",
+#   "site_type"=>"Subproperty",
+#   "longitude"=>"-73.864223918",
+#   "borough"=>"Bronx",
+#   "latitude"=>"40.890848989",
+#   "park_site_name"=>"227th St. Plgd"
+# }
 
 
+#1. Define a method `address`, that takes a hash as an argument.
+#This method should return the value of the address key in the hash.
 
+#2. Define a method called `borough` that takes a hash as an argument.
+# This method should return the value of the borough key in the hash.
+
+#3. Define a method called `all_keys` that takes a hash as an argument.
+#This method should print out each key in the hash. 
+#You should use the `each_key` method to iterate over your hash and print out each key.
+
+#4. Define a method called `all_data` that takes a hash as an argument.
+#This method should print out each value in the hash.
+#You should use the `each_value` method to iterate over your hash and print out each value.
+
+#5. Define a method called `key_and_data` that takes a hash as an argument.
+#This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
+#You should use the `each` method to iterate over your hash to print out that sentence
